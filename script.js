a7bf93ae40ad37dae47efb89f2075f4158425e5b
Daniel Galuardi <dan.galuardi@gmail.com>
2022-10-10 12:24:11 -0400

    Square div with equal column and rows

--- script.js
+++ script.js
@@ -3,14 +3,16 @@
 
 function divGrid(number){
     let row =0;
+    const width = 500/number
     number = number * number  // Area of divs in grid
     const container = document.querySelector('#container');
     for (row; row < number; row++){
         let newRow = document.createElement('div');
         newRow.setAttribute('id',row)
         newRow.setAttribute('class', 'divGrid')
+        newRow.style.cssText = 'width: ' + width + 'px'
         container.appendChild(newRow);
     }
 }
 
-divGrid(16)
+divGrid(32)
